3040ec138891842f9ece2259f7b3d2071eb072ac
ck <ck@laptop01.local>
2021-04-26 13:10:03 +0200

    remove output

--- checksummer.ps1
+++ checksummer.ps1
@@ -16,8 +16,7 @@ param	(
 foreach ($filenames in $filename)
 {
 	Write-Host "Getting checksum from $filenames" -ForegroundColor Yellow
-	Get-FileHash -Algorithm $Algorithmtype $filenames
-	Get-FileHash -Algorithm $Algorithmtype $filenames | Out-File checkedfiles.txt
+	Get-FileHash -Algorithm $Algorithmtype $filenames	
 	'--------------------------------------------------'
 
 }
