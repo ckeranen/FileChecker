6071741d9de24509c59561ca2ac6ebcef6b44f74
ck <ck@laptop01.local>
2021-04-26 10:44:24 +0200

    added output

--- checksummer.ps1
+++ checksummer.ps1
@@ -1,8 +1,9 @@
 $filenames = (Get-Content checksumfiles.txt)
 foreach ($filename in $filenames)
 {
-	Write-Host "$filename" -ForegroundColor Yellow
+	Write-Host "Getting SHA256 checksum from $filename" -ForegroundColor Yellow
 	Get-FileHash -Algorithm SHA256 $filename
+	Get-FileHash -Algorithm SHA256 $filename | Out-File \tmp\checkedfiles.txt -Append
 	'--------------------------------------------------'
 
 }
