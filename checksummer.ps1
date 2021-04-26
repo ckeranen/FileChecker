034dcd2bda0ada74c389584cbff31138a764069e
ck <ck@laptop01.local>
2021-04-26 10:30:50 +0200

    Start testing

---
+++ checksummer.ps1
@@ -0,0 +1,7 @@
+$filenames = (Get-Content checksumfiles.txt)
+foreach ($filename in $filenames)
+{
+	Write-Host "$filename" -ForegroundColor Yellow
+	Get-FileHash -Algorithm SHA256 $filename
+	'--------------------------------------------------'
+}
