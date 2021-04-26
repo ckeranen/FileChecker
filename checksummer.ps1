34a0b844e4c267ba39077de3fd9406e9c4f3434b
ck <ck@laptop01.local>
2021-04-26 10:36:52 +0200

    add some space

--- checksummer.ps1
+++ checksummer.ps1
@@ -4,4 +4,5 @@ foreach ($filename in $filenames)
 	Write-Host "$filename" -ForegroundColor Yellow
 	Get-FileHash -Algorithm SHA256 $filename
 	'--------------------------------------------------'
+
 }
