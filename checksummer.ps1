8a9538109cc80f7bff5e267e1869a11c4e860798
ck <ck@laptop01.local>
2021-04-26 11:31:17 +0200

    make file to a script

--- checksummer.ps1
+++ checksummer.ps1
@@ -1,9 +1,15 @@
-$filenames = (Get-Content checksumfiles.txt)
+param	(
+		[Parameter(Mandatory=$true,HelpMessage="Write filename")]
+		[string[]]$filenames,
+		[string]$Algorithmtype = 'SHA256'
+	)
+
+#$filenames = (Get-Content checksumfiles.txt)
 foreach ($filename in $filenames)
 {
-	Write-Host "Getting SHA256 checksum from $filename" -ForegroundColor Yellow
-	Get-FileHash -Algorithm SHA256 $filename
-	Get-FileHash -Algorithm SHA256 $filename | Out-File \tmp\checkedfiles.txt -Append
+	Write-Host "Getting checksum from $filename" -ForegroundColor Yellow
+	Get-FileHash -Algorithm $Algorithmtype $filename
+	Get-FileHash -Algorithm $Algorithmtype $filename | Out-File \tmp\checkedfiles.txt -Append
 	'--------------------------------------------------'
 
 }
